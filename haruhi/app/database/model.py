2bcad60f1970afa406c68d13b302433cbd0e2bce
firedial <42674772+firedial@users.noreply.github.com>
2022-08-28 15:11:08 +0900

    色々テーブル作成

--- haruhi/app/database/model.py
+++ haruhi/app/database/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Date
 from sqlalchemy.orm import relationship
 
 from database.database import Base, engine
@@ -27,6 +27,77 @@ class KindElement(Base):
     updated_at = Column(DateTime)
 
     category = relationship("KindCategory", uselist=False)
+    balance = relationship("Balance", uselist=True)
+
+
+class PurposeCategory(Base):
+    __tablename__ = "m_purpose_category"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(20), unique=True, index=True, nullable=False)
+    description = Column(String(20), index=True, nullable=False)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    element = relationship("PurposeElement", uselist=True)
+
+
+class PurposeElement(Base):
+    __tablename__ = "m_purpose_element"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(20), unique=True, index=True, nullable=False)
+    description = Column(String(20), index=True, nullable=False)
+    category_id = Column(Integer, ForeignKey("m_purpose_category.id"), index=True, nullable=False)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    category = relationship("PurposeCategory", uselist=False)
+    balance = relationship("Balance", uselist=True)
+
+
+class PlaceCategory(Base):
+    __tablename__ = "m_place_category"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(20), unique=True, index=True, nullable=False)
+    description = Column(String(20), index=True, nullable=False)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    element = relationship("PlaceElement", uselist=True)
+
+
+class PlaceElement(Base):
+    __tablename__ = "m_place_element"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(20), unique=True, index=True, nullable=False)
+    description = Column(String(20), index=True, nullable=False)
+    category_id = Column(Integer, ForeignKey("m_place_category.id"), index=True, nullable=False)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    category = relationship("PlaceCategory", uselist=False)
+    balance = relationship("Balance", uselist=True)
+
+
+class Balance(Base):
+    __tablename__ = "m_balance"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    amount = Column(Integer, index=True, nullable=False)
+    item = Column(String(50), nullable=False)
+    kind_element_id = Column(Integer, ForeignKey("m_kind_element.id"), index=True, nullable=False)
+    purpose_element_id = Column(Integer, ForeignKey("m_purpose_element.id"), index=True, nullable=False)
+    place_element_id = Column(Integer, ForeignKey("m_place_element.id"), index=True, nullable=False)
+    date = Column(Date, index=True, nullable=False)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    kind = relationship("KindElement", uselist=False)
+    purpose = relationship("PurposeElement", uselist=False)
+    place = relationship("PlaceElement", uselist=False)
 
 
 Base.metadata.create_all(bind=engine)
