82cedfc935d02bc1896400b9c99d1c572321a313
firedial <42674772+firedial@users.noreply.github.com>
2022-09-19 22:05:41 +0900

    ルーティングの設定

--- nagisa/app/src/App.js
+++ nagisa/app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/test" element={<Test />}></Route>
+      </Routes>
+    </Router>
   );
 }
+
+const Home = () => (
+  <h2>Home</h2>
+);
+const Test = () => (
+  <h2>Test</h2>
+);
 
 export default App;
