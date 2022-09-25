bd5daea39927acce2758d67124b22335f93dafb9
firedial <42674772+firedial@users.noreply.github.com>
2022-09-25 20:14:13 +0900

    ヘッダーの作成

--- nagisa/app/src/App.tsx
+++ nagisa/app/src/App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 
 import { BalanceList, BalanceCreate } from './page/balances'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/balances" element={<BalanceList />}></Route>
-        <Route path="/balances/create" element={<BalanceCreate />}></Route>
-        <Route path="/test/:n" element={<Test name={"hoge"} />}></Route>
-      </Routes>
-    </Router>
+    <>
+      <header>
+        <div className='header-title'>midas-lycoris</div>
+        <a href="/">Home</a>
+        <a href="/balances">Balance</a>
+      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/balances" element={<BalanceList />}></Route>
+          <Route path="/balances/create" element={<BalanceCreate />}></Route>
+          <Route path="/test/:n" element={<Test name={"hoge"} />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
